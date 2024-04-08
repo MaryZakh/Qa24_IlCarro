5c9fc91350c02ecc5a06e7abd01c09b170ea8ce9
Maria Z <z0559882272@gmail.com>
2024-04-08 20:00:03 +0300

    date

--- src/main/java/manager/HelperCar.java
+++ src/main/java/manager/HelperCar.java
@@ -5,7 +5,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class HelperCar extends HelperBase{
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class HelperCar extends HelperBase {
     public HelperCar(WebDriver wd) {
         super(wd);
     }
@@ -17,17 +20,17 @@ public class HelperCar extends HelperBase{
 
     public void fillCarForm(Car car) {
         typeLocation(car.getLocation());
-        type(By.id("make"),car.getManufacture());
-        type(By.cssSelector("#model"),car.getModel());
-        type(By.xpath("//input[@id='year']"),car.getYear());
-        select(By.id("fuel"),car.getFuel());
-        type(By.id("seats"),String.valueOf(car.getSeats()));
-       // type(By.id("seats"),car.getSeats()+"");
-        type(By.id("class"),car.getCarClass());
-        type(By.id("serialNumber"),car.getCarRegNumber());
+        type(By.id("make"), car.getManufacture());
+        type(By.cssSelector("#model"), car.getModel());
+        type(By.xpath("//input[@id='year']"), car.getYear());
+        select(By.id("fuel"), car.getFuel());
+        type(By.id("seats"), String.valueOf(car.getSeats()));
+        // type(By.id("seats"),car.getSeats()+"");
+        type(By.id("class"), car.getCarClass());
+        type(By.id("serialNumber"), car.getCarRegNumber());
         //type(By.id("price"),String.valueOf(car.getPrice()));
-        type(By.id("price"),car.getPrice()+"");
-        type(By.id("about"),car.getAbout());
+        type(By.id("price"), car.getPrice() + "");
+        type(By.id("about"), car.getAbout());
     }
 
     private void select(By locator, String option) {
@@ -40,7 +43,7 @@ public class HelperCar extends HelperBase{
     }
 
     private void typeLocation(String location) {
-        type(By.id("pickUpPlace"),location);
+        type(By.id("pickUpPlace"), location);
         click(By.cssSelector("div.pac-item"));
     }
 
@@ -50,5 +53,71 @@ public class HelperCar extends HelperBase{
 
     public void attachPhoto(String link) {
         wd.findElement(By.cssSelector("#photos")).sendKeys(link);
+    }
+
+    public void searchCurrentMonth(String city, String dateFrom, String dateTo) {
+        typeCity(city);
+        click(By.id("dates"));
+        //"4/25/2024","4/28/2024"
+
+        String[] from = dateFrom.split("/");///["4"]["25"]["2024"]
+
+        String locatorFrom = "//div[text()=' " + from[1] + " ']";
+
+        String[] to = dateTo.split("/");///["4"]["28"]["2024"]
+
+        click(By.xpath(locatorFrom));
+        click(By.xpath("//div[text()=' " + to[1] + " ']"));
+
+
+    }
+
+    private void typeCity(String city) {
+        type(By.id("city"), city);
+        click(By.cssSelector("div.pac-item"));
+    }
+
+    public boolean isListOfCarsAppeared() {
+        return isElementPresent(By.cssSelector("a.car-container"));
+    }
+
+    public void searchCurrentYear(String city, String dateFrom, String dateTo) {
+        typeCity(city);
+        click(By.id("dates"));
+
+        //"4/27/2024", "6/28/2024"
+        LocalDate now = LocalDate.now();
+        System.out.println(now); //2024-04-07
+        int year = now.getYear();
+        int month = now.getMonthValue();
+        int day = now.getDayOfMonth();
+
+        LocalDate from = LocalDate
+                .parse(dateFrom, DateTimeFormatter.ofPattern("M/d/yyyy"));//2024-04-27
+//        LocalDate from1 = LocalDate
+//                .parse("2013:23/05", DateTimeFormatter.ofPattern("yyyy:dd/MM"));
+//
+//        System.out.println(from1);
+
+        LocalDate to = LocalDate
+                .parse(dateTo, DateTimeFormatter.ofPattern("M/d/yyyy"));
+
+        int diffMonth = from.getMonthValue() - month;
+        if (diffMonth > 0) {
+            clickNextMonthBtn(diffMonth);
+        }
+        click(By.xpath("//div[text()=' " + from.getDayOfMonth() + " ']"));
+        diffMonth = to.getMonthValue() - from.getMonthValue();
+        if (diffMonth > 0) {
+            clickNextMonthBtn(diffMonth);
+        }
+        click(By.xpath("//div[text()=' " + to.getDayOfMonth() + " ']"));
+
+    }
+
+    private void clickNextMonthBtn(int diffMonth) {
+        for (int i = 0; i < diffMonth; i++) {
+            click(By.cssSelector("button[aria-label='Next month']"));
+        }
     }
 }
